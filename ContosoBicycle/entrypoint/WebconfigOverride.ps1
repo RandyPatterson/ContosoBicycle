6d38cbf08350b5fa6c0fef7211f96d377b95a312
Randy Patterson <randy@randypatterson.com>
2021-03-05 11:59:29 -0500

    Update WebconfigOverride.ps1 to add or update keys

--- ContosoBicycle/entrypoint/WebconfigOverride.ps1
+++ ContosoBicycle/entrypoint/WebconfigOverride.ps1
@@ -2,7 +2,11 @@ param (
     [string]$webConfig = "c:\inetpub\wwwroot\Web.config"
 )
 
+
+
+
 iisreset stop
+Write-Host
 
 $doc = (Get-Content $webConfig) -as [Xml];
 $modified = $FALSE;
@@ -12,21 +16,35 @@ $connectionStringPrefix = "CONNSTR_";
 
 Get-ChildItem env:* | ForEach-Object {
     if ($_.Key.StartsWith($appSettingPrefix)) {
+        $modified = $TRUE;
         $key = $_.Key.Substring($appSettingPrefix.Length);
         $appSetting = $doc.configuration.appSettings.add | Where-Object {$_.key -eq $key};
         if ($appSetting) {
             $appSetting.value = $_.Value;
             Write-Host "Replaced appSetting" $_.Key $_.Value;
-            $modified = $TRUE;
+
+        } else {
+            $newElement = $doc.CreateElement("add");
+            $add = $doc.configuration.appSettings.AppendChild($newElement);
+            $add.SetAttribute("key",$key);
+            $add.SetAttribute("value", $_.Value);
+            Write-Host "Added appSetting" $_.Key $_.Value;
+           
         }
     }
     if ($_.Key.StartsWith($connectionStringPrefix)) {
+        $modified = $TRUE;
         $key = $_.Key.Substring($connectionStringPrefix.Length);
         $connStr = $doc.configuration.connectionStrings.add | Where-Object {$_.name -eq $key};
         if ($connStr) {
             $connStr.connectionString = $_.Value;
             Write-Host "Replaced connectionString" $_.Key $_.Value;
-            $modified = $TRUE;
+        } else {
+            $newElement = $doc.CreateElement("add");
+            $add = $doc.configuration.connectionStrings.AppendChild($newElement);
+            $add.SetAttribute("name",$key);
+            $add.SetAttribute("connectionString", $_.Value);
+            Write-Host "Added connectionString" $_.Key $_.Value;
         }
     }
 }
