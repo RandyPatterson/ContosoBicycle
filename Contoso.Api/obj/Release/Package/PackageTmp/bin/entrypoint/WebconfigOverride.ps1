aeac7cb81908e3870f50bb808d8dc77eedfb05b1
Randy Patterson <randy@randypatterson.com>
2021-03-05 15:54:34 -0500

    Added .gitignore

--- Contoso.Api/obj/Release/Package/PackageTmp/bin/entrypoint/WebconfigOverride.ps1
+++ Contoso.Api/obj/Release/Package/PackageTmp/bin/entrypoint/WebconfigOverride.ps1
@@ -2,7 +2,8 @@ param (
     [string]$webConfig = "c:\inetpub\wwwroot\Web.config"
 )
 
-iisreset stop
+stop-service w3svc -pa
+Write-Host
 
 $doc = (Get-Content $webConfig) -as [Xml];
 $modified = $FALSE;
@@ -12,21 +13,35 @@ $connectionStringPrefix = "CONNSTR_";
 
 Get-ChildItem env:* | ForEach-Object {
     if ($_.Key.StartsWith($appSettingPrefix)) {
+        $modified = $TRUE;
         $key = $_.Key.Substring($appSettingPrefix.Length);
         $appSetting = $doc.configuration.appSettings.add | Where-Object {$_.key -eq $key};
         if ($appSetting) {
             $appSetting.value = $_.Value;
             Write-Host "Replaced appSetting" $_.Key $_.Value;
-            $modified = $TRUE;
+
+        } else {
+            $newElement = $doc.CreateElement("add");
+            $add = $doc.configuration.appSettings.AppendChild($newElement);
+            $add.SetAttribute("key",$key);
+            $add.SetAttribute("value", $_.Value);
+            Write-Host "Added appSetting" $_.Key $_.Value;
+           
         }
     }
     if ($_.Key.StartsWith($connectionStringPrefix)) {
+        $modified = $TRUE;
         $key = $_.Key.Substring($connectionStringPrefix.Length);
         $connStr = $doc.configuration.connectionStrings.add | Where-Object {$_.name -eq $key};
         if ($connStr) {
             $connStr.connectionString = $_.Value;
             Write-Host "Replaced connectionString" $_.Key $_.Value;
-            $modified = $TRUE;
+        } else {
+            $newElement = $doc.CreateElement("add");
+            $add = $doc.configuration.connectionStrings.AppendChild($newElement);
+            $add.SetAttribute("name",$key);
+            $add.SetAttribute("connectionString", $_.Value);
+            Write-Host "Added connectionString" $_.Key $_.Value;
         }
     }
 }
@@ -35,4 +50,4 @@ if ($modified) {
     $doc.Save($webConfig);
 }
 
-iisreset start
+start-service w3svc -pa
--- ContosoBicycle/obj/Release/Package/PackageTmp/bin/entrypoint/Startup.ps1
+++
@@ -1,2 +0,0 @@
-c:\WebconfigOverride.ps1  -webConfig c:\inetpub\wwwroot\Web.config
-C:\ServiceMonitor.exe w3svc
